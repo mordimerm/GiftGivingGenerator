f4c220e5d419e38af3d1cbf7ee12662aa4f6b4f2
Adrian Czekaj <aczekaj.mat@gmail.com>
2021-12-22 10:11:00 +0100

    #13 filled column in db>Event with values 'true'

---
+++ GiftGivingGenerator.API/Migrations/20211222085039_FillColumnIsActiveInEventsWithValuesTrue.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GiftGivingGenerator.API.Migrations
+{
+    public partial class FillColumnIsActiveInEventsWithValuesTrue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            update Events
+            set IsActive = 'true'
+            where IsActive IS NULL;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
