acba93b5149cc3b99ed6e58480100051921323cf
Adrian Czekaj <aczekaj.mat@gmail.com>
2022-02-16 11:02:58 +0100

    #53 added creating person without checking if there is one with the same email address (assume that there can be few persons with the same email)

--- GiftGivingGenerator.API/Controllers/PersonsController.cs
+++ GiftGivingGenerator.API/Controllers/PersonsController.cs
@@ -1,4 +1,5 @@
 ﻿using GiftGivingGenerator.API.DataTransferObject.Person;
+using GiftGivingGenerator.API.Entities;
 using GiftGivingGenerator.API.Repositories.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,20 +9,28 @@ namespace GiftGivingGenerator.API.Controllers;
 [Route("[controller]")]
 public class PersonsController : ControllerBase
 {
-	private readonly IPersonRepository _repository;
+	private readonly IPersonRepository _personRepository;
 
-	public PersonsController(IPersonRepository repository)
+	public PersonsController(IPersonRepository personRepository)
 	{
-		_repository = repository;
+		_personRepository = personRepository;
+	}
+
+	[HttpPost]
+	public ActionResult Create([FromBody] CreatePersonDto dto)
+	{
+		var person = Person.Create(dto.Name, dto.Email);
+		_personRepository.Insert(person);
+		return Created($"/Persons/{person.Id}", person);
 	}
 
 	[HttpPut("{id}/Name")]
 	public ActionResult ChangePersonName([FromRoute] Guid id, [FromBody] EditPersonDto get)
 	{
-		var person = _repository.Get(id);
+		var person = _personRepository.Get(id);
 		person.ChangeName(get.Name);
 
-		_repository.Update(person);
+		_personRepository.Update(person);
 		return Ok();
 	}
 }
