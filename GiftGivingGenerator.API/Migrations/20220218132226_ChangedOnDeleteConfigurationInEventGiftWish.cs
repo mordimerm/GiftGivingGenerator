9c24ddc35424a8e77fc9bf08f700865d9a760010
Adrian Czekaj <aczekaj.mat@gmail.com>
2022-02-18 14:27:25 +0100

    #58 pushed migration

---
+++ GiftGivingGenerator.API/Migrations/20220218132226_ChangedOnDeleteConfigurationInEventGiftWish.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GiftGivingGenerator.API.Migrations
+{
+    public partial class ChangedOnDeleteConfigurationInEventGiftWish : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GiftWish_Events_EventId",
+                table: "GiftWish");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GiftWish_Events_EventId",
+                table: "GiftWish",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GiftWish_Events_EventId",
+                table: "GiftWish");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GiftWish_Events_EventId",
+                table: "GiftWish",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
